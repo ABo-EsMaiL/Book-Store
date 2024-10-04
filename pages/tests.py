ce954eeb603fe48e3bd4984dfb1548a9390e579f
ABo EsMaiL <ahmedaboesmail16@gmail.com>
2024-10-04 04:47:43 +0300

    ch6

--- pages/tests.py
+++ pages/tests.py
@@ -38,8 +38,12 @@ class HomepageTests(SimpleTestCase):
         self.assertEqual(self.response.status_code, 200)
     def test_homepage_template(self): 
         self.assertTemplateUsed(self.response, "home.html")
-    def test_homepage_contains_correct_html(self): 
-        self.assertContains(self.response, "home page")
+    def test_homepage_contains_correct_html(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, "home.html")
+        self.assertContains(self.response, "<h1>Welcome to the Book Store</h1>")
+        self.assertContains(self.response, "<a href=\"/accounts/login/\">Login</a>")
+        self.assertContains(self.response, "<a href=\"/accounts/signup/\">Sign Up</a>")
     def test_homepage_does_not_contain_incorrect_html(self): 
         self.assertNotContains(self.response, "Hi there! I should not be on the page.")
         
