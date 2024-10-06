ae42072841eb3377df55914c693c34fd38683142
ABo EsMaiL <ahmedaboesmail16@gmail.com>
2024-10-06 15:39:06 +0300

    ch12

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,15 +3,28 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-
-# Register your models here.
-
 CustomUser = get_user_model()
+
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ['username', 'email', 'is_superuser']
     
+    # Explicitly define fieldsets to prevent inheritance issues
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    # Explicitly define add_fieldsets for creating a new user
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- books/admin.py
+++ books/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Review
 
 # Register your models here.
 
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 1
 
 class BookAdmin(admin.ModelAdmin):
+    inlines = [ReviewInline]
     list_display = ('title', 'author', 'price')
 
 
