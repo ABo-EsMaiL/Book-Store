3a69213ec5fc62863ddf45283cd44c547118fc71
Ahmed AbdElHady Mohamed Esmail <123252905+ABo-EsMaiL@users.noreply.github.com>
2024-10-13 22:47:45 +0300

    Update models.py

--- books/models.py
+++ books/models.py
@@ -14,7 +14,7 @@ class Book(models.Model):
     price = models.DecimalField(max_digits=6,
                                 decimal_places=2)
     # cover = models.ImageField(upload_to='covers/', blank=True, null=True)
-    cover = CloudinaryField('covers/', blank=True, null=True)
+    cover = CloudinaryField('covers', blank=True, null=True)
 
     def __str__(self):
         return self.title
