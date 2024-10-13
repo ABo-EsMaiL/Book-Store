a568e2270f8eaa3fb065fe6a764df0fb4102adff
ABo EsMaiL <ahmedaboesmail16@gmail.com>
2024-10-13 17:32:45 +0300

    Deployment to Railway

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default = False)
 
-ALLOWED_HOSTS = [".railway.app", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["book-store-production-b7cc.up.railway.app", "localhost", "127.0.0.1"]
 
 
 # Application definition
